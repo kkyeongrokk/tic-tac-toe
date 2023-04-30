ac32a0575cd0d3fec73bc53ee0a07506d2c17064
kkyeongrokk <sisiko8@naver.com>
2023-04-30 13:35:40 -0700

    defined constants and state variables

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*----- constants -----*/
+const COLORS = {
+  0: "white",
+  1: "black",
+  "-1": "pink",
+};
+
+/*----- state variables -----*/
+let board; // array of 7 column arrays
+let turn; // 1 or -1
+let winner; // null = no winner; 1 or -1 = winner; 'T' = Tie game
+
+/*----- cached elements  -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
